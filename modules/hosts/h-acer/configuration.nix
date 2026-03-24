dc472dc299634c335248245145e2b19b78190242
HTunne <hazzatun@gmail.com>
2026-03-24 22:34:57 +0000

    feat(h-acer): Add config for h-acer

--- modules/hosts/h-acer/configuration.nix
+++ modules/hosts/h-acer/configuration.nix
@@ -6,9 +6,170 @@
   flake.nixosConfigurations.h-acer = inputs.nixpkgs.lib.nixosSystem {
     modules = [
       self.nixosModules.h-acer
+      inputs.neovim.nixosModules.neovim
     ];
   };
 
   flake.nixosModules.h-acer = {pkgs, ...}: {
+
+    wrappers.neovim.enable = true;
+
+    nixpkgs.overlays = [
+      (_final: prev: {
+        jellyfin-web = prev.jellyfin-web.overrideAttrs (
+          _finalAttrs: _previousAttrs: {
+            installPhase = ''
+              runHook preInstall
+
+              # this is the important line
+              sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+
+              mkdir -p $out/share
+              cp -a dist $out/share/jellyfin-web
+
+              runHook postInstall
+            '';
+          }
+        );
+      })
+    ];
+
+    services.deluge = {
+      enable = true;
+    };
+
+    services.slskd = {
+      enable = false;
+    };
+
+    services.jellyfin = {
+    user = "h";
+      enable = true;
+      openFirewall = true;
+    };
+
+    services.jellyseerr = {
+      enable = true;
+    };
+
+    services.sonarr = {
+      enable = true;
+    };
+
+    services.radarr = {
+      enable = true;
+    };
+
+    services.readarr = {
+      enable = true;
+    };
+
+    services.bazarr = {
+      enable = true;
+    };
+
+    services.lidarr = {
+      enable = true;
+    };
+
+    services.audiobookshelf = {
+      enable = true;
+    };
+
+    # sabnzbd?
+
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "h-acer"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "Europe/London";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_GB.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+      layout = "gb";
+      variant = "";
+    };
+
+    # Configure console keymap
+    console.keyMap = "uk";
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.h = {
+      isNormalUser = true;
+      description = "h";
+      extraGroups = ["networkmanager" "wheel" "optical"];
+      packages = with pkgs; [];
+    };
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+    	makemkv
+    ];
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    programs.neovim.enable = true;
+    programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+    programs.git.enable = true;
+
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "25.11"; # Did you read the comment?
   };
 }
